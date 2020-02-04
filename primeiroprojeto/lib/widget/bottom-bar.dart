79fc7727197268c2ee3cb0e3dc77f8743a23f393
Bruno Eduardo <bruno.soares@kepha.com.br>
2020-02-04 11:17:36 -0300

    iniciando estrutura

---
+++ primeiroprojeto/lib/widget/bottom-bar.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+///
+/// Widget da barra de navegação inferior
+///
+/// [author: Bruno Eduardo]
+///
+class BottomBar extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      backgroundColor: Colors.black87,
+      items: <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          icon: Icon(Icons.people, color: Colors.white),
+          activeIcon: Icon(Icons.people, color: Colors.white30),
+          title: Text('Usuários', style: TextStyle(color: Colors.white)),
+        ),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.person_add, color: Colors.white),
+            activeIcon: Icon(Icons.person_add, color: Colors.white30),
+            title: Text('Salvos', style: TextStyle(color: Colors.white))
+        )
+      ],
+    );
+  }
+
+}
